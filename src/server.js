9a139a304f278ee94800695a06816b847141159c
Eloi <eloi.chrysanthe@gmail.com>
2022-12-08 22:56:09 +0200

    changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-import config from './config/config';
+// import config from './config/config';
 const currentConfig = config[process.env.NODE_ENV];
-import log from './logger';
+// import log from './logger';
 const app = require('./index');
+const config = require('./config/config');
 const { port } = currentConfig;
 const server = app.listen(port, () => {
   console.info('you server is running well 🌎🔥🔥🔥', port);
