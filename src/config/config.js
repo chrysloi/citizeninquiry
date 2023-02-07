e2f047823e5bbbb9ce5eee8b95d97e833231c737
Eloi <eloi.chrysanthe@gmail.com>
2023-02-07 07:38:20 +0200

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -7,14 +7,13 @@ module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
     url: process.env.MONGODB_URL,
     port: process.env.PORT,
-    token: process.env.ADMIN_TOKEN,
-    talking: process.env.TALKING_APIKEY,
+    talking: process.env.TWILIO_AUTH_TOKEN,
   },
   production: {
     JWT_SECRET: process.env.JWT_SECRET,
     url: process.env.MONGODB_URL,
     port: process.env.PORT,
     logging: false,
-    talking: process.env.TALKING_APIKEY,
+    talking: process.env.TWILIO_AUTH_TOKEN,
   },
 };
